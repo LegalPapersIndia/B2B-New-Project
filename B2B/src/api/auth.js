702cbd1fc0f5741bfe69e2979a0a6494a8febbbc
LegalPapersIndia <udit@documentsindia.com>
2026-05-20 18:13:53 +0530

    update API URL for production

--- B2B/src/api/auth.js
+++ B2B/src/api/auth.js
@@ -1,17 +1,31 @@
+// import axios from "axios";
+
+// const API = "http://localhost:5000/api";
+
+// export const registerSeller = async (data) => {
+//   return await axios.post(
+//     `${API}/auth/seller/register`,
+//     data
+//   );
+// };
+
+// export const loginSeller = async (data) => {
+//   return await axios.post(
+//     `${API}/auth/seller/login`,
+//     data
+//   );
+// };
+
+
+
 import axios from "axios";
 
-const API = "http://localhost:5000/api";
+const API = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const registerSeller = async (data) => {
-  return await axios.post(
-    `${API}/auth/seller/register`,
-    data
-  );
+  return await axios.post(`${API}/auth/seller/register`, data);
 };
 
 export const loginSeller = async (data) => {
-  return await axios.post(
-    `${API}/auth/seller/login`,
-    data
-  );
+  return await axios.post(`${API}/auth/seller/login`, data);
 };
